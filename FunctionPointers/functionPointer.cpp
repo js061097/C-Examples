70a27c4b6edb53f31668755c0fbbfb502c9fb667
Jason Stanley <js41074546@gmail.com>
2022-01-04 14:07:32 -0600

    Create functionPointer.cpp

---
+++ FunctionPointers/functionPointer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void helloworld(){
+    std::cout<<"Hello World!"<<endl;
+}
+
+int main()
+{
+    helloworld();
+    void(*ptr1)() = helloworld;
+    ptr1();
+    auto func1 = helloworld;
+    func1();
+    auto func2 = &helloworld;
+    func2();
+    return 0;
+}
